8cca205402abc67f876a590ad1b7c0a6b59c1582
Shakil Mahmud <almahmudruet17@gmail.com>
2021-11-28 10:01:14 +0600

    last one

--- src/Firebase/Initialize.js
+++ src/Firebase/Initialize.js
@@ -2,14 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBO4vW427gvoBUHk7RPhgW7LdjNIehSf58",
-    authDomain: "react-firebase-f7093.firebaseapp.com",
-    projectId: "react-firebase-f7093",
-    storageBucket: "react-firebase-f7093.appspot.com",
-    messagingSenderId: "529312335159",
-    appId: "1:529312335159:web:127221b9f20ad56f5886b2",
-    measurementId: "G-J87JLMD12P"
-  };
+  apiKey: "AIzaSyDmtWygLDLFnY6s1INs6aLRmdgS05YN038",
+  authDomain: "mongo-firebase-3d0fa.firebaseapp.com",
+  projectId: "mongo-firebase-3d0fa",
+  storageBucket: "mongo-firebase-3d0fa.appspot.com",
+  messagingSenderId: "391890041591",
+  appId: "1:391890041591:web:efe04766ab071dcf3871f2"
+};
 
 const initializeAuth=()=>{
     initializeApp(firebaseConfig);
